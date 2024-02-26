307d87dbc2d330388cb6bdafc239428bfa0ae393
jboadirgt <113521930+jboadirgt@users.noreply.github.com>
2024-02-26 12:30:22 +0000

    ui updated commit

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -38,7 +38,7 @@ export const LoginButton = ({
 
   if (mode === "modal") {
     return (
-      <Dialog className="!px-0">
+      <Dialog>
         <DialogTrigger asChild={asChild}>{children}</DialogTrigger>
         <DialogContent className="p-0  w-max  flex items-center justify-center bg-transparent border-none !z-[10000000] max-h-[96vh] overflow-y-auto no-scrollbar shadow-lg">
           {formType === "login" ? <LoginForm /> : <RegisterForm />}
